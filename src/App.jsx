db65b1e2c7ecf502f10f9268de965838baa59968
Arslan <91261682+4rchi-nv@users.noreply.github.com>
2024-07-10 10:46:07 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import reactImg from './assets/react-core-concepts.png'
-import { useState } from 'react';
-import { CORE_CONCEPTS } from './assets/data';
-const words = ['Фундаментальные', 'Четкие', 'Крутые'];
-
-function Cards({ title, image, description }) {
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
-
 function App() {
-  // сamelCase
-  // PascalCase
-  // snake_case
-  // kebab-case
-  const [word, setWord] = useState(words[0]);
   return (
-    <>
+    <div>
       <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>{word} Основы React</h1>
-        <ol>
-          <li>Название компонента должно начаться с большой буквы</li>
-          <li>Если в названии больше одного слова, то пишется через PascalCase</li>
-          <li>Принято использовать семантико-верные названия(Header, MyHeader)</li>
-        </ol>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
       </header>
       <main>
-        <section id="core-concepts">
-          <ul>
-            {CORE_CONCEPTS.map(el =>
-            (<Cards {...el} />
-            ))}
-          </ul>
-        </section>
-        <button onClick={e => setWord(() => words[Math.floor(Math.random() * words.length)])}>click</button>
+        <h2>Time to get started!</h2>
       </main>
-
-    </>
+    </div>
   );
 }
 
